4337fc8f87314402bdda3caa50678c2f953cf69f
Steve Johnson <steve@steveasleep.com>
2019-11-17 21:22:13 -0800

    Encapsulate pedal stuff

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,7 @@
       <button @click="travel">Next Product &rarr;</button>
     </nav>
 
-    <article v-if="pedal">
-      <Gfx
-        v-bind:aleaSavedState="aleaSavedState"
-        v-bind:name="pedal.name"
-        v-bind:brand="pedal.brand"
-        v-bind:purpose="pedal.purpose"
-        v-bind:color="pedal.color"
-      ></Gfx>
-
-      <h2 class="Brand">{{ pedal.brand }}</h2>
-      <h1 class="PedalName">{{ pedal.name }}</h1>
-      <h3 class="PedalPrice">{{ pedal.price }}</h3>
-
-      <h3>{{ pedal.subtitle }}</h3>
-      <p v-for="(t, i) in pedal.texts" v-bind:key="i">{{ t }}</p>
-
-      <div class="BuyButton"><a href="https://quickfiction.bandcamp.com" target="_">Buy Now</a></div>
-    </article>
+    <Entry v-if="seed" v-bind:seed="seed"></Entry>
 
     <footer>
       <strong>Tools used to make this site:</strong>
@@ -62,6 +45,7 @@ import Alea from "alea";
 import { ref, computed, onMounted, onBeforeUpdate } from '@vue/composition-api';
 
 import Gfx from './Gfx.vue';
+import Entry from './Entry.vue';
 
 import makeImprovGenerators from './makeImprovGenerators';
 import queryString from 'query-string';
@@ -69,41 +53,15 @@ import queryString from 'query-string';
 export default {
   components: {
     Gfx,
+    Entry,
   },
   setup() {
     const seed = ref(null);
-    const pedal = ref(null);
-    const aleaSavedState = ref(null);
-    
-    let _alea = null;
 
     function derive(shouldSetHash) {
-      const alea = new Alea(seed.value);
-      const {descGen, subGen} = makeImprovGenerators(alea);
-      const model = {};
-      const name = descGen.gen('name', model);
-      const subtitle = subGen.gen('root', model);
-      const desc = descGen.gen('root', model);
-      const brand = descGen.gen('brand', model);
-      console.log(model);
-      aleaSavedState.value = alea.exportState();
-      pedal.value = {
-        name,
-        subtitle,
-        brand,
-        purpose: subGen.gen('purpose', model),
-        color: descGen.gen('color', model).toLowerCase(),
-        texts: desc.split('\n\n').filter((s) => s),
-        price: descGen.gen('price', model),
-      };
       if (shouldSetHash) {
         window.location.hash = `seed=${seed.value}`;
       }
-      console.log(model);
-
-      // for (let i=0; i<10; i++) {
-      //   console.log(descGen.gen('name'));
-      // }
     }
 
     function travel() {
@@ -125,10 +83,6 @@ export default {
       }
     }
 
-    onBeforeUpdate(() => {
-      _alea = Alea.importState(aleaSavedState.value);
-    });
-
     onMounted(() => {
       if (!reactToHash(queryString.parse(window.location.hash), true)) {
         travel();
@@ -141,9 +95,7 @@ export default {
 
     return {
       seed,
-      pedal,
       travel,
-      aleaSavedState,
     }
   }
 }
